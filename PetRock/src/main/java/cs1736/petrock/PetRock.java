f6e3fcf580b7e5e4a42dd2bf8240a641d3e54c9f
ims30 <ims30@pitt.edu>
2025-01-28 14:19:28 -0500

    Initial Stub out with variables and methods

--- PetRock/src/main/java/cs1736/petrock/PetRock.java
+++ PetRock/src/main/java/cs1736/petrock/PetRock.java
@@ -2,7 +2,27 @@
 package cs1736.petrock;
 public class PetRock {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    //Variables 
+    String name = "";
+    String[] mood = {"Happy", "Bored", "Tired", "Sad"};
+    int hunger ;
+    int boredom;
+    int energy;
+    
+    //Methods 
+    public void FeedRock(){
+        System.out.println("Feed rock");
+    }
+    
+    public void PlayWithRock(){
+         System.out.println("Play with rock");
+    }
+    
+    public void PolishRock(){
+         System.out.println("polish rock");
+    }
+    
+    public void UpdateMood(){
+         System.out.println("update mood");
     }
 }
