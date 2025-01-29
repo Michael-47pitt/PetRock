6e63ba449f2f64e4443e3b9eb5a28fadbd132ce2
Michael-47pitt <MGP47@pitt.edu>
2025-01-29 12:28:26 -0500

    Add files via upload

---
+++ PetRock/src/main/java/cs1736/petrock/PetRock.java
@@ -0,0 +1,28 @@
+
+package cs1736.petrock;
+public class PetRock {
+
+    //Variables 
+    String name = "";
+    String[] mood = {"Happy", "Bored", "Tired", "Sad"};
+    int hunger ;
+    int boredom;
+    int energy;
+    
+    //Methods 
+    public void FeedRock(){
+        System.out.println("Feed rock");
+    }
+    
+    public void PlayWithRock(){
+         System.out.println("Play with rock");
+    }
+    
+    public void PolishRock(){
+         System.out.println("polish rock");
+    }
+    
+    public void UpdateMood(){
+         System.out.println("update mood");
+    }
+}
