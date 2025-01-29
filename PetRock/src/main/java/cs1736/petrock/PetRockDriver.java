4a9e1f85d0357eb8f49897caeb7d880cf863d7be
Michael <03finn@gmail.com>
2025-01-29 13:14:59 -0500

    Stubbed out main uses of driver program, such as loop

--- PetRock/src/main/java/cs1736/petrock/PetRockDriver.java
+++ PetRock/src/main/java/cs1736/petrock/PetRockDriver.java
@@ -1,4 +1,5 @@
 package cs1736.petrock;
+import java.util.Scanner;
 
 /**
  *
@@ -8,6 +9,43 @@ public class PetRockDriver
 {
     public static void main(String[] args) 
     {
-    
+        int userInput;
+        
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Pet Rock Driver");
+        
+        do
+        {
+            System.out.println("1. Feed rock");
+            System.out.println("2. Play with rock");
+            System.out.println("3. Polish rock");
+            System.out.println("4. Check status");
+            System.out.println("5. Quit");
+            
+            System.out.println("Enter your number choice");
+            userInput=scanner.nextInt();
+            
+            if(userInput==1)
+            {
+                System.out.println("You chose choice 1");
+            }
+            else if(userInput==2)
+            {
+                System.out.println("You chose choice 2");
+            }
+            else if(userInput==3)
+            {
+                System.out.println("You chose choice 3");
+            }
+            else if(userInput==4)
+            {
+                System.out.println("You chose choice 4");
+            }
+            else if(userInput==5)
+            {
+                System.out.println("Quitting Program");
+                break;
+            }
+        }while(userInput!=5);
     }  
 }
