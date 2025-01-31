f59dd43bad3a234676aa69a5536fc6fc3068b5cd
Michael <03finn@gmail.com>
2025-01-31 12:26:01 -0500

    Added constant min/max values

--- PetRock/src/main/java/cs1736/petrock/PetRock.java
+++ PetRock/src/main/java/cs1736/petrock/PetRock.java
@@ -4,24 +4,37 @@ public class PetRock {
     //Variables 
     String name = "";
     String[] mood = {"Happy", "Bored", "Tired", "Sad"};
-    int hunger ;
-    int boredom;
-    int energy;
+    int hunger ; // High hunger = bad
+    int boredom; // High boredom = bad
+    int energy; // Low energy = bad
+    
+    // Constant values for maximum and minimum values
+    public static final int MAX_HUNGER=10;
+    public static final int MAX_BOREDOM=10;
+    public static final int MAX_ENERGY=10;
+    public static final int MIN_HUNGER=10;
+    public static final int MIN_BOREDOM=10;
+    public static final int MIN_ENERGY=10;
     
     //Methods 
-    public void FeedRock(){
+    public void FeedRock()
+    {
         System.out.println("Feed rock");
     }
     
-    public void PlayWithRock(){
+    public void PlayWithRock()
+    {
          System.out.println("Play with rock");
     }
     
-    public void PolishRock(){
+    public void PolishRock()
+    {
          System.out.println("polish rock");
+         
     }
     
-    public void UpdateMood(){
+    public void UpdateMood()
+    {
          System.out.println("update mood");
     }
 }
