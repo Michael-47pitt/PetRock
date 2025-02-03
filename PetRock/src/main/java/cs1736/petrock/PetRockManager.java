a4e21b78dfe7b9c035b8d24cf4eefb2271b23fc1
gabe-lutz <gjl23@pitt.edu>
2025-02-03 12:41:21 -0500

    PolishRock() and UpdateMood() updated

--- PetRock/src/main/java/cs1736/petrock/PetRockManager.java
+++ PetRock/src/main/java/cs1736/petrock/PetRockManager.java
@@ -21,18 +21,25 @@ public class PetRockManager {
     }
 
     public void PolishRock(PetRock p) {
-        hunger--;
-        boredom--;
-        energy++;
+        int petRockHunger = p.getHunger();
+        int petRockBoredom = p.getBoredom();
+        int petRockEnergy = p.getEnergy();
+        int newHunger = petRockHunger - 1;
+        int newBoredom = petRockBoredom - 1;
+        int newEnergy = petRockEnergy + 1;
+        p.setHunger(newHunger);
+        p.setBoredom(newBoredom);
+        p.setEnergy(newEnergy);
+        
         polishRockCounter++;
         System.out.println("polish rock");
     }
 
     public void UpdateMood(PetRock p) {
-        System.out.println("Name: " + this.name);
-        System.out.println("Mood: " + this.mood);
-        System.out.println("Hunger: " + this.hunger);
-        System.out.println("Boredom: " + this.boredom);
-        System.out.println("Energy: " + this.energy);
+        System.out.println("Name: " + p.getName());
+        System.out.println("Mood: " + p.getMood());
+        System.out.println("Hunger: " + p.getHunger());
+        System.out.println("Boredom: " + p.getBoredom());
+        System.out.println("Energy: " + p.getEnergy());
     }
 }
