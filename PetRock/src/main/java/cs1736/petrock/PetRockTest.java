246f1c7f0c999d9e1e0d88c6d391a4c88e9a95bd
Michael <03finn@gmail.com>
2025-02-03 12:52:06 -0500

    Added line in PetRockTest to check github is working

--- PetRock/src/main/java/cs1736/petrock/PetRockTest.java
+++ PetRock/src/main/java/cs1736/petrock/PetRockTest.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cs1736.petrock;
 
-/**
- *
- * @author 03fin
- */
-public class PetRockTest {
-    
+public class PetRockTest 
+{
+    System.out.println("This is a test file!")
 }
