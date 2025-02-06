3a51ca62ddf2971dc27d0e685a0e1ed5a7034f28
Michael <03finn@gmail.com>
2025-02-06 17:42:56 -0500

    Added JUnit test file in src/test/java directory. Needs merged and possibly refactored to fit tests for updated Manager on PetRockManager branch.

--- PetRock/src/test/java/cs1736/petrock/PetRockTest.java
+++ PetRock/src/test/java/cs1736/petrock/PetRockTest.java
@@ -2,7 +2,9 @@ package cs1736.petrock;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Disabled;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class PetRockTest {
 
@@ -52,6 +54,30 @@ public class PetRockTest {
         assertEquals(6, petRock.getEnergy(), "Energy should increase by 1 after polishing");
         assertEquals(4, petRock.getBoredom(), "Boredom should decrease by 1 after polishing");   
     }
+    
+    @Test
+    //@Disabled("Not easily testable due to print lines in display method")
+    public void testDisplayStatus_SuccessfulDisplay()
+    {
+        // Setting expected output 
+        String expectedOutput=
+        "Name: null\n" +
+        "Mood: " + petRock.getMood() + "\n" +
+        "Hunger: 5\n" +
+        "Boredom: 5\n" +
+        "Energy: 5\n";
+        
+        // Capturing output
+        ByteArrayOutputStream printOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(printOut));
+        
+        // Performing Display
+        PetRockManager.DisplayStatus(petRock);
+        
+        // Restoring systemout to System out instead of print out
+        System.setOut(System.out);
+        
+        // Checking expected values
+        assertEquals(expectedOutput,printOut.toString()); // Problem here, the expected and actual seem to be equivalent but JUnit doesn't see it that way?
+    }
 }
-
-
